615ffd62f50f03c193e7b0cdc287b4646c61ed23
drussillo <daverussillo7@gmail.com>
2025-11-07 12:02:14 -0600

    Added window and glad init

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -4,7 +4,61 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World" << std::endl;
+
+  /*****************************************************************/
+  // Initialize GLFW
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW\n";
+    return -1;
+  }
+
+  // Set OpenGL version and profile (3.3 core)
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  /*****************************************************************/
+  // create window and initialize glad
+  int res_w = 1920;
+  int res_h = 1080;
+  bool fullscreen = true;
+
+  GLFWmonitor* monitor = fullscreen ? glfwGetPrimaryMonitor() : nullptr;
+
+  // Create a window
+  GLFWwindow* window = glfwCreateWindow(res_w, res_h, "FlappyPigeon", monitor, nullptr);
+  if (!window) {
+    std::cerr << "Failed to create GLFW window\n";
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  // Load OpenGL function pointers using GLAD
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+      std::cerr << "Failed to initialize GLAD\n";
+      return -1;
+  }
+
+  glViewport(0, 0, res_w, res_h);
+  /*****************************************************************/
+
+  // main loop
+  while (!glfwWindowShouldClose(window)) {
+    /*****************************************/
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    // Game p3
+    // p3.mainloop()
+
+    /*****************************************/
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // Clean up
+  glfwDestroyWindow(window);
+  glfwTerminate();
 
   return 0;
 }
